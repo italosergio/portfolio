0fd2faf84ff2597d2d849af0199645d0d15ccd7e
Italo Sergio <italosergio@graduate.org>
2023-05-28 23:27:19 -0300

    feat: side nav bar implemented on not found page

--- app/frontend/src/pages/NotFound.jsx
+++ app/frontend/src/pages/NotFound.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import SideNavBar from '../components/SideNavBar';
 
 function NotFoundPage() {
   return (
     <>
       <Header />
+      <SideNavBar />
       <div>
         <h1>404 - Página não encontrada</h1>
         <p>A página que você está procurando não foi encontrada.</p>
