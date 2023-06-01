a6967150efdc8fbbcffa135f6a3442090c41f28e
Italo Sergio <italosergio@graduate.org>
2023-06-01 03:21:48 -0300

    add: contacts

--- app/frontend/src/pages/Contact.jsx
+++ app/frontend/src/pages/Contact.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import Header from '../components/Header'
-import SideNavBar from '../components/SideNavBar';
-import Developing from '../components/Developing';
 import video from '../video/contact-bg.mp4'
+import Header from '../components/Header'
+import CardContact from '../components/CardContact';
+import SideNavBar from '../components/SideNavBar';
+import whatsappIcon from '../images/icon-whatsapp.svg'
+import linkedinIcon from '../images/icon-linkedin.svg';
+import githubIcon from '../images/icon-github.svg';
+import instagramIcon from '../images/icon-instagram.svg';
 
 function Contact() {
-
+  const whatsappLink = "https://api.whatsapp.com/send?phone=5588994693031&text=Olá,%20estou%20enviando%20esta%20mensagem%20atraves%20do%20seu%20portfolio."
+  const linkedinLink = "https://www.linkedin.com/in/italosergio"
+  const githubLink = "https://www.github.com/italosergio"
+  const instagramLink = "https://www.instagram.com/italosergio"
   return (
     <>
       <video autoPlay muted loop className='contact-video fade-in'>
@@ -14,7 +21,13 @@ function Contact() {
       </video>
       <Header page='/contact' />
       <SideNavBar />
-      <Developing />
+      <div className='contact-container-bg'></div>
+      <div className='contact-content'>
+        <CardContact title="WhatsApp" icon={whatsappIcon} link={whatsappLink} />
+        <CardContact title="LinkedIn"icon={linkedinIcon} link={linkedinLink} />
+        <CardContact title="GitHub" icon={githubIcon} link={githubLink} />
+        <CardContact title="Instagram" icon={instagramIcon} link={instagramLink} />
+      </div>
     </>
   );
 }
