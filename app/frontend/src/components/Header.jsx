5bf2f35fbd3ec103e3744c03633ca9630af691c6
Italo Sergio <italosergio@graduate.org>
2023-06-05 01:13:25 -0300

    feat: improve header fonts and add logo image

--- app/frontend/src/components/Header.jsx
+++ app/frontend/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import NavBar from './NavBar';
+import logo from '../images/logo1.png'
 
 function Header(props) {
   return (
     <header className='header fade-in'>
       <div className='header-title'>
-        <h1>Ítalo Chaves</h1>
-        <span>Desenvolvedor Full Stack</span>
+        <img src={logo} alt="Logo do titulo" className='logo'/>
+        <div className='header-name'>
+          <h1>{'<'} ÍTALO SÉRGIO {'/>'}</h1><span>DEV FULL STACK</span>
+        </div>
       </div>
       <NavBar page={props.page} />
     </header>
