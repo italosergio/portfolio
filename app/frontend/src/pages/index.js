ea477acf3c6d637c9feb52f5d9320e1f2fa7e933
Ítalo Sérgio <italosergio@mail.com>
2025-09-25 21:46:38 -0300

    instalação portfolio v3

--- app/frontend/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import Home from './Home';
-import NotFoundPage from './NotFound';
-import Projects from './Projects';
-import Contact from './Contact';
-
-export {Home, NotFoundPage, Projects, Contact};
