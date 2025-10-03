efc786c9e9f9a77d1a4515af1c8e86068f6e1d5a
Ítalo Sérgio <italosergio@mail.com>
2025-10-03 14:03:00 -0300

    remove temporariamente sessao de serviços

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <Header />
       <Hero />
       <About />
-      <Projects />
+      {/* <Projects /> */}
       <Skills />
       <Contact />
       <Footer />
